f99c044cd9d71144482a49456daa30a480341743
Jenni <jennimmartin@outlook.com>
2026-02-10 17:29:59 +0100

    Links added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <>
       <footer className="bg-black">
-        <div className="h-96">
-          <p className="text-white">I am a footer</p>
-        </div>
+        <h5 className="text-center py-6 text-white">
+          <span className="hover:text-charcoal-500 duration-300">
+            <Link to="/legal">Legal</Link>
+          </span>
+          {" | "}
+          &copy; {new Date().getFullYear()}
+          {" | "}
+          <span>
+            Created by{" "}
+            <a
+              href="https://jennimmartin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-charcoal-500 duration-300"
+            >
+              Jenni M Martin
+            </a>
+          </span>
+        </h5>
       </footer>
     </>
   );
